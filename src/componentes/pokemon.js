60e59024133ec756e17b60a00ab310b369a266c4
Daniel Londoño <iva.dan.lon.pin@gmail.com>
2025-04-24 02:14:57 +0000

    Actualizacion

---
+++ src/componentes/pokemon.js
@@ -0,0 +1,9 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Pokemon() {
+    return (
+        <View>
+            <Text>pokemon</Text>
+        </View>
+    );
+}
---
+++ src/componentes/usuario.js
@@ -0,0 +1,9 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Usuario() {
+    return (
+        <View>
+            <Text>usuario</Text>
+        </View>
+    );
+}
